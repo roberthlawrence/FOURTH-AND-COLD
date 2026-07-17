c19346b268eeb1b9236cff5835629d26d76392b5
roberthlawrence <roberthlawrence@gmail.com>
2026-07-17 02:03:49 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // Paste your Firebase web app config here.
 // Firebase console → Project settings → Your apps → Web app → Config
 export const firebaseConfig = {
-  apiKey: "PASTE_ME",
-  authDomain: "PASTE_ME.firebaseapp.com",
-  projectId: "PASTE_ME",
-  storageBucket: "PASTE_ME.appspot.com",
-  messagingSenderId: "PASTE_ME",
-  appId: "PASTE_ME"
+ apiKey: "AIzaSyAvvssZ_XL4pvv4784k40W-aapzSNsmijc",
+  authDomain: "fourth-and-cold.firebaseapp.com",
+  projectId: "fourth-and-cold",
+  storageBucket: "fourth-and-cold.firebasestorage.app",
+  messagingSenderId: "692456470696",
+  appId: "1:692456470696:web:7f5612c0b095f62743ea9d"
 };
